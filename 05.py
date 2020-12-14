1749d268622a28922624edfdb7ffefd8c7f94ce0
Almaz Kunpeissov <almaz.kun@gmail.com>
2020-12-14 15:11:31 +0900

    day 05

--- 05.py
+++ 05.py
@@ -38,3 +38,110 @@ FFFBBBFRRR: row 14, column 7, seat ID 119.
 BBFFBBFRLL: row 102, column 4, seat ID 820.
 As a sanity check, look through your list of boarding passes. What is the highest seat ID on a boarding pass?
 """
+path = "05_input.txt"
+file = "test.txt"
+
+all_codes = []
+
+with open(file, "r") as f:
+    for l in f.readlines():
+        all_codes.append(l.rstrip())
+
+
+class Code:
+    def __init__(self, code):
+        self.code = code
+        self.row = None
+        self.col = None
+    
+    def half(self, arr, half):
+        l = len(arr)
+        h2 = l//2
+        if not l % 2:
+            if half == "F" or half == "L":
+                return arr[:h2]
+            if half == "B" or half == "R":
+                return arr[h2:]
+        else:
+            return arr
+        
+    def get_row(self, arr=None):
+        if arr is None:
+            arr = [x for x in range(0, 128)]
+        while len(arr) > 1:
+            for half in self.code[:7]:
+                arr = self.half(arr, half)
+        self.row = int(arr[0])
+        return self.row
+    
+    def get_col(self, arr=None):
+
+        if arr is None:
+            arr = [x for x in range(0, 8)]
+        while True:
+            for half in self.code[7:]:
+                arr = self.half(arr, half)
+            if len(arr) == 1:
+                break
+        self.col = int(arr[0])
+        return self.col
+    
+    def get_seat_id(self):
+        self.get_row()
+        self.get_col()
+        self.seat_id = self.row * 8 + self.col
+        return self.seat_id
+         
+        
+        
+tests = [
+    (70, 7, 567),
+    (14, 7, 119),
+    (102, 4, 820),
+]
+
+def maxest():
+    ids = []
+    for code in all_codes:
+        c = Code(code)
+        c.get_seat_id()
+        ids.append(c.seat_id)
+    return ids
+
+
+def a_ids():
+    a_ids = []
+    for r in [x for x in range(1,127)]:
+        for c in [x for x in range(1, 6)]:
+            i = r * 8 + c
+            a_ids.append(i)
+    return a_ids
+"""
+--- Part Two ---
+
+Ding! The "fasten seat belt" signs have turned on. Time to find your seat.
+
+It's a completely full flight, so your seat should be the only missing boarding pass in your list. However, there's a catch: some of the seats at the very front and back of the plane don't exist on this aircraft, so they'll be missing from your list as well.
+
+Your seat wasn't at the very front or back, though; the seats with IDs +1 and -1 from yours will be in your list.
+
+What is the ID of your seat?
+
+Your puzzle answer was 669.
+
+Both parts of this puzzle are complete! They provide two gold stars: **
+
+At this point, you should return to your Advent calendar and try another puzzle.
+"""
+        
+a_ = a_ids()
+ii = maxest()
+
+def dif():
+    nots = []
+    for a in a_:
+        if a not in ii:
+            nots.append(a)
+    print(nots)
+
+dif() #669
